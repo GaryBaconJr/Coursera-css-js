c69aa4877f5bf40986462bd29b6ead677c539796
GaryBaconJr <garybaconjr@yahoo.com>
2016-06-21 13:54:00 -0400

    Add files via upload

--- Mod4Solution/SpeakGoodBye.js
+++ Mod4Solution/SpeakGoodBye.js
@@ -19,7 +19,7 @@
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 	byeSpeaker.speak = function () {
-	  console.log(speakWord + " " + helloSpeaker.name);
+	  console.log(speakWord + " " + byeSpeaker.name);
 	}
 	window.byeSpeaker = byeSpeaker;
 
